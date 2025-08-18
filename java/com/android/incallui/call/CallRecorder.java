27eef869f43905f918575b4cea9c590dd18ccc9a
AXP.OS <aosbot@axpos.org>
2025-08-18 17:05:01 +0000

    ER: unrestricted call recording\nF_CALLREC: CALLRECORDER2
    Change-Id: I53fc80ca10c476a76eace0a1008ebdd3d421cb37

--- java/com/android/incallui/call/CallRecorder.java
+++ java/com/android/incallui/call/CallRecorder.java
@@ -94,6 +94,16 @@ public class CallRecorder implements CallList.Listener {
   }
 
   public boolean canRecordInCurrentCountry() {
+      // extendrom - any call recording
+      int ER_CallRecordingEnabled = Settings.Secure.getInt(
+      context.getContentResolver(),
+      "extendrom_call_recording", 0);
+
+      if (ER_CallRecordingEnabled == 1) {
+          return true;
+      }
+      // END: extendrom - any call recording
+
       if (!isEnabled()) {
           return false;
       }
